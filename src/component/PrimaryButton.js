eed840b9494d626c104967088bef62d10ec2638d
usoni11 <u.soni@onefc.com>
2020-12-16 20:19:19 +0530

    Button alignment fixed

--- src/component/PrimaryButton.js
+++ src/component/PrimaryButton.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const PrimaryButton = styled.button`
-  display: inline-block;
   padding: 10px;
   background-color: var(--violet);
   border-color: var(--violet);
